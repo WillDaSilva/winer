ff392ad5bcfafe160e0ad2576eb53d56bca52200
AidanLB <31937900+AidanLB@users.noreply.github.com>
2017-12-02 16:12:12 -0500

    Add server-side websocket

--- server/winer_server.py
+++ server/winer_server.py
@@ -7,18 +7,30 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 from flask import Flask, url_for, render_template
+from flask_socketio import SocketIO, emit
 app = Flask (__name__)
 logHandler = RotatingFileHandler('winer.log', maxBytes=10000, backupCount=1)
 logHandler.setLevel(logging.INFO)
 app.logger.addHandler(logHandler)
 chat_log = []
+socketio = SocketIO(app)
 
 @app.route('/')
 def render_chat_page():
     load_chat_log()
-    return render_template('chat.html',log: chat_log)
+    return render_template('chat.html')
 
-@app.route('/message')
+@socketio.on('message')
+def handle_message(message):
+    message = json.loads(message)
+
+    logMessage(message.body)
+    emit('message',message,broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    emit('message', json.dumps({'history': chat_log[:3]}), broadcast=False )
+
 
 def load_chat_log():
     with open('chat_log.json', 'r') as chat_log_file:
@@ -49,7 +61,11 @@ def logMessage(
     chat_log.append({'body': body, 'timestamp': timestamp, 'alias': alias})
 
 
+
 def cleanup():
     save_chat_log()
 
 atexit.register(cleanup)
+
+if __name__ == '__main__':
+    socketio.run(app)
