f06ba1d1cdd7ffee93f47b0072b1e479a2a7187d
Will Da Silva <will@willdasilva.xyz>
2017-12-02 15:44:01 -0500

    Add client-side message handling

--- server/static/client.js
+++ server/static/client.js
@@ -6,8 +6,42 @@ ws.onopen = function(event) {
 
 ws.onmessage = function(messageData) {
     let message = JSON.parse(messageData.data);
-    // TODO: render message on page
+    let messageBox = document.getElementsById('messageBox');
+    handleMessageUpdate(message)
 };
+
+let handleMessageUpdate = function(message) {
+    colour = message.playerColour; // Server tells client what colour they are
+    let textColour = message.colour || 'black';
+    let backgroundColour = message.backgroundColour || 'rgba(255, 255, 255, 0)';
+    messageBox = document.getElementById('messageBox');
+    if (this.loadedHistory) {
+        let messageDiv = document.createElement('div');
+        messageDiv.style.color = textColour;
+        messageDiv.style.backgroundColor = backgroundColour;
+        messageDiv.innerHTML = message.body;
+        let needToScroll =  messageBox.scrollTop + messageBox.clientHeight ===
+                            messageBox.scrollHeight;
+        messageBox.appendChild(messageDiv);
+        if (needToScroll) {
+            messageBox.scrollTop = messageBox.scrollHeight;
+        }
+    }
+    else {
+        for (historyElement of message.history) {
+            let messageDiv = document.createElement('div');
+            messageDiv.innerHTML = historyElement.body;
+            messageDiv.style.color = historyElement.colour || 'black';
+            messageDiv.style.backgroundColor = historyElement.backgroundColour || 'rgba(255, 255, 255, 0)';
+            messageBox.appendChild(messageDiv);
+        }
+        // Scroll to the bottom of the message box
+        messageBox.scrollTop = messageBox.scrollHeight;
+        this.loadedHistory = true;
+    }
+};
+handleMessageUpdate.loadedHistory = false; // Flag to indicate first call
+
 
 function handleSubmitButton() {
     messageBody = getElementsByName('textBox').trim();
@@ -15,3 +49,7 @@ function handleSubmitButton() {
         ws.send(JSON.stringify({body: messageBody}));
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('textBox').addEventListener('keypress', handleKeyPress);
+});
